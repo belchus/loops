45516cba516c34d138c39edae3d31dcf7b9be902
Camila Arena <arenabelu@gmail.com>
2022-07-13 20:52:26 -0300

    first commit

---
+++ clase3/js/index.js
@@ -0,0 +1,10 @@
+
+let textoAMostrar = prompt("Ingrese las palabras que quiera ver en el texto-Ingrese ESC cuando quiera terminar")
+let cadena
+
+while (textoAMostrar != "ESC") {
+ cadena = cadena + textoAMostrar
+ 
+ textoAMostrar = prompt("Ingrese la siguiente palabra")
+}
+console.log(cadena)
